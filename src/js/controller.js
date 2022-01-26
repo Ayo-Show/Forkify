e9cc4c09bac38820fcc5a68689b17e5600447c1e
Ayo-Show <sowunmi30@gmail.com>
2022-01-26 15:03:35 -0500

    update app to listen to load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,12 +30,17 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    
     // Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc3e'
+      
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -157,4 +162,8 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
