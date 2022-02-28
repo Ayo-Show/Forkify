4cd52604ecd91c254621c62998a2d1173731a61c
Ayo-Show <sowunmi30@gmail.com>
2022-02-28 16:01:20 -0500

    Implemented search results function

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,10 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 // import { async } from 'regenerator-runtime';
-
-const recipeContainer = document.querySelector('.recipe');
 
 
 
@@ -14,32 +12,43 @@ const recipeContainer = document.querySelector('.recipe');
 
 ///////////////////////////////////////
 
-
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
-
+    
     if (!id) return;
     recipeView.renderSpinner();
 
     // Loading recipe
     await model.loadRecipe(id);
-    
 
     // 2: Rendering recipe
     recipeView.render(model.state.recipe);
-
-    
   } catch (err) {
     recipeView.renderError();
     // console.error(err);
   }
 };
 
-const init = function() {
-  recipeView.addHandlerRender(controlRecipes);
-}
-init();
+const controlSearchResults = async function () {
+  try {
+    // Get search query
+    const query = searchView.getQuery();
+    if(!query) return;
 
+    // load search results
+    await model.loadSearchResults(query);
+
+    // render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
