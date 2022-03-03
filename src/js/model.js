cc91c7248c95af84bf4da5dc3e213808dd2d8e03
Ayo-Show <sowunmi30@gmail.com>
2022-03-02 19:02:42 -0500

    implemented function to update recipe servings

--- src/js/model.js
+++ src/js/model.js
@@ -52,19 +52,25 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
-    
   } catch (err) {
     console.error(`${err} 💥💥💥💥`);
     throw err;
   }
 };
 
-export const getSearchResultsPage = function(page = state.search.page) {
+export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
 
   const start = (page - 1) * state.search.resultsPerPage;
   const end = page * state.search.resultsPerPage;
 
   return state.search.results.slice(start, end);
-}
+};
 
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  })
+
+  state.recipe.servings = newServings;
+};
