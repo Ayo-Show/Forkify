487c86d8dad95d33b613e9c06b1e19b3e326c99c
Ayo-Show <sowunmi30@gmail.com>
2022-03-06 08:07:03 -0500

    implemented function to render recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '82ed33bd-655f-48e5-9ee4-87c423f463aa';
+export const MODAL_CLOSE_SEC = 2.5;
